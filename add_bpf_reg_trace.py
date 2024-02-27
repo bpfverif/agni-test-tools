c31f0d4d5af0fc76b57c0804b616b35524694ccd
bhivam <shivamkajaria@gmail.com>
2024-02-27 01:53:18 -0500

    py script to add bpf trace to kernel

---
+++ add_bpf_reg_trace.py
@@ -0,0 +1,86 @@
+"""
+Program Description:
+"""
+
+import os
+import sys
+import shutil
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 add_bpf_reg_trace.py <kernel_directory_path>")
+        sys.exit(0)
+
+    kernel_directory_path = sys.argv[1]  # test that kernel path exists
+    bpf_trace_path = "./kernel_prep_files/bpf_state.h"
+
+    if kernel_directory_path[-1] != '/':
+        kernel_directory_path += '/'
+
+    verifier_source_path = kernel_directory_path + "kernel/bpf/verifier.c"
+
+    shutil.copyfile(bpf_trace_path, kernel_directory_path +
+                    "include/trace/events/bpf_state.h")
+
+    verifier_source = open(verifier_source_path, "r")
+    verifier_source_lines = verifier_source.readlines()
+
+    for idx, line in enumerate(verifier_source_lines):
+        if line[0] != "#":
+            continue
+
+        verifier_source_lines.insert(
+            idx, "#include <trace/events/bpf_state.h>\n"
+        )
+        verifier_source_lines.insert(
+            idx, "#define CREATE_TRACE_POINTS\n"
+        )
+
+        break
+
+    do_check_start_line = -1
+    for idx, line in enumerate(verifier_source_lines):
+        if "int do_check(" in line:
+            do_check_start_line = idx
+            break
+
+    idx = do_check_start_line + 2
+    num_open_braces = 1
+
+    while num_open_braces > 0:
+        num_open_braces += verifier_source_lines[idx].count("{")
+        num_open_braces -= verifier_source_lines[idx].count("}")
+        idx += 1
+
+    while "return" not in verifier_source_lines[idx]:
+        idx -= 1
+
+    do_check_end_line = idx
+
+    verifier_source_lines.insert(do_check_end_line,
+                                 """
+    trace_bpf_state(regs[BPF_REG_1].var_off.value,
+                  regs[BPF_REG_1].var_off.mask,
+                  regs[BPF_REG_1].smin_value,
+                  regs[BPF_REG_1].smax_value,
+                  regs[BPF_REG_1].umin_value,
+                  regs[BPF_REG_1].umax_value,
+                  regs[BPF_REG_1].s32_min_value,
+                  regs[BPF_REG_1].s32_max_value,
+                  regs[BPF_REG_1].u32_min_value,
+                  regs[BPF_REG_1].u32_max_value
+    );
+"""
+                                 )
+
+    verifier_source.close()
+    os.remove(verifier_source_path)
+
+    verifier_source = open(verifier_source_path, "w")
+    verifier_source.writelines(verifier_source_lines)
+    verifier_source.close()
+
+
+if __name__ == "__main__":
+    main()
